c4ab42fad22f0202479f3fa7266f2e672d906953
jinzi39 <jinzi@snares-05.cs.wisc.edu>
2019-04-28 18:02:15 -0500

    update neuralnet

--- neuralnet.py
+++ neuralnet.py
@@ -14,6 +14,8 @@ from numpy import genfromtxt
 
 import time 
 
+import seaborn as sns
+
 def norm(x, train_stats):
   return (x - train_stats['mean']) / train_stats['std']
 
@@ -36,7 +38,7 @@ def nn():
 
     #raw_dataset = pd.read_csv(directory, names=column_names)
     #dataset = raw_dataset.copy()
-    print(dataset.tail())
+    #print(dataset.tail())
 
     # split into train and test
     train_dataset = dataset.sample(frac=0.8,random_state=0)
@@ -48,7 +50,7 @@ def nn():
     train_stats = train_dataset.describe()
     train_stats.pop("favorite_count")
     train_stats = train_stats.transpose()
-    print(train_stats)
+    #print(train_stats)
 
     # get tables
     train_labels = train_dataset.pop('favorite_count')
@@ -57,17 +59,16 @@ def nn():
     # to do: normalize
     normed_train_data = norm(train_dataset, train_stats)
     normed_test_data = norm(test_dataset, train_stats)
-
-    print(normed_train_data.tail())
-    print(normed_test_data.tail())
+    #print(normed_train_data.tail())
+    #print(normed_test_data.tail())
 
     # start time 
     start = time.time()
 
-
     # build model
     model = keras.Sequential([
     layers.Dense(64, activation=tf.nn.relu, input_shape=[len(train_dataset.keys())]),
+    layers.Dense(64, activation=tf.nn.relu),
     layers.Dense(64, activation=tf.nn.relu),
     layers.Dense(64, activation=tf.nn.relu),
     layers.Dense(1)
@@ -79,35 +80,34 @@ def nn():
                 optimizer=optimizer,
                 metrics=['mean_absolute_error', 'mean_squared_error'])
 
-    model.summary()
+    #model.summary()
        
-    example_batch = normed_train_data[:10]
-    example_result = model.predict(example_batch)
-    print(example_result)
+    #example_batch = normed_train_data[:10]
+    #example_result = model.predict(example_batch)
+    #print(example_result)
 
-    # The patience parameter is the amount of epochs to check for improvement
+    # Early stopping. The patience parameter is the amount of epochs to check for improvement
     early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
     
     # train model
     EPOCHS = 100
-
     history = model.fit(
         normed_train_data, train_labels,
         epochs=EPOCHS, validation_split = 0.2, verbose=0, callbacks=[early_stop])
-
     hist = pd.DataFrame(history.history)
     hist['epoch'] = history.epoch
-    print(hist.tail())
+    #print(hist.tail())
 
     # test model
     loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=0)
-
     print("Testing set Mean Abs Error: {:5.2f}".format(mae))
     
     # end time 
     end = time.time()
-
     print("total runtime: ", end-start)
+
+    #print("{:5.2f}".format(mae) + str(end-start))
+
 if __name__== "__main__":
-  nn()
+    nn()
 
